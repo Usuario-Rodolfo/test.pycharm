928861a7e310663655eaf1ee5da18ef8915a28d2
Usuario-Rodolfo <srrodolfo00@gmail.com>
2025-03-06 22:58:13 -0300

    Adicionando código inicial

---
+++ ExibirVariavelString.py
@@ -0,0 +1,9 @@
+código = int(input("Digite o código de funcionário "))
+nome = input("Digite o nome do funcionário: ")
+salario = float(input("Informe o Salario "))
+ativo = True
+
+print("Código: %d "% código)
+print("Nome: %s "% nome)
+print("Salario: %.2f " % salario)
+print("Ativo: %r " % ativo)
