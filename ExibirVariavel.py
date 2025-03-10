240bca67dab4e23b5ee888f470ef2be488f907b3
Usuario-Rodolfo <srrodolfo00!gmail.com>
2025-03-10 12:28:51 -0300

    Adicionando ExibirVariavel.py

--- ExibirVariavel.py
+++ ExibirVariavel.py
@@ -1,6 +1,6 @@
-codigo = 105
-nome = "José Santana"
-salario = 1650.00
+codigo = int(input("Digite o código do funcionário "))
+nome = input("Digite o nome do funcionário ")
+salario = float(input("Informe o salario "))
 ativo = True
 
 print("codigo: %d "% codigo)
